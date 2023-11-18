c5a403f3eff364aa6a97476bd5917a13b096d6ed
ahmedghaleb1993 <ahmedghaleb199322@gmail.com>
2023-11-18 11:28:22 +0100

    add adsense

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {process.env.NODE_ENV === 'production' && (
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9187309347349975"
+          crossorigin="anonymous"></script>
+        )}
+      </head>
       <body className={` ${inter.className} bg-[#EEEEEE]`}>
         <Navbar />
         {children}
